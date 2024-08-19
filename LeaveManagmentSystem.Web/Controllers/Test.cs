981657271c6785f7948fff5b6074a8a1926d904c
Ana Pavlovic <49585841+pavlovicana@users.noreply.github.com>
2024-08-19 20:54:41 +0200

    Add project files.

---
+++ LeaveManagmentSystem.Web/Controllers/Test.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagmentSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagmentSystem.Web.Controllers
+{
+    public class Test : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC Master",
+                DateOfBirth = new DateTime(1994,05, 21)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagmentSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagmentSystem.Web.Models
+{
+    public class TestViewModel
+    {
+
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
