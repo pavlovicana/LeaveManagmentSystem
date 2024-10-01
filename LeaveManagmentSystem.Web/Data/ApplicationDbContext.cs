e9c6fa1a35cd54918885b095254ada9ca1c2aa32
Ana Pavlovic <49585841+pavlovicana@users.noreply.github.com>
2024-10-01 17:34:47 +0200

    Complited Leave Managment Module

--- LeaveManagmentSystem.Web/Data/ApplicationDbContext.cs
+++ LeaveManagmentSystem.Web/Data/ApplicationDbContext.cs
@@ -9,5 +9,8 @@ namespace LeaveManagmentSystem.Web.Data
             : base(options)
         {
         }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
     }
 }
---
+++ LeaveManagmentSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagmentSystem.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(150)")]
+        public string Name { get; set; }
+
+        public int NumberOfDays { get; set; }
+
+
+
+    }
+}
